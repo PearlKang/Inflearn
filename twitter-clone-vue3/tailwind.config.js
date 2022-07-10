467f78a9c9acfc9f7d5d821ed387608cb02bca62
BenKang <rksk1588@gmail.com>
2022-07-10 21:31:36 +0900

    2022.07.10

--- twitter-clone-vue3/tailwind.config.js
+++ twitter-clone-vue3/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1DA1F2",
+        dark: "#1A91DA",
+        light: "#A5D0F5",
+        lighter: "#D4E5F4",
+      },
+    },
   },
   plugins: [],
 };
