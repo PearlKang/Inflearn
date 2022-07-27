3a6fdb747dc32072f9aec3cec79c3e580d738d23
BenKang <rksk1588@gmail.com>
2022-07-28 00:46:53 +0900

    2022.07.28

--- twitter-clone-vue3/src/pages/Register.vue
+++ twitter-clone-vue3/src/pages/Register.vue
@@ -1,7 +1,62 @@
-<template></template>
+<template>
+  <div class="flex flex-col items-center space-y-4 mt-10">
+    <i
+      :class="`fab fa-twitter text-4xl text-primary ${
+        loading ? 'animate-bounce' : ''
+      }`"
+    ></i>
+    <span class="text-2xl font-bold">뜨위떠 회원가입</span>
+    <input
+      v-model="email"
+      type="text"
+      class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none"
+      placeholder="이메일"
+    />
+    <input
+      v-model="username"
+      type="text"
+      class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none"
+      placeholder="아이디"
+    />
+    <input
+      v-model="password"
+      type="text"
+      class="rounded w-96 px-4 py-3 border border-gray-300 focus:ring-2 focus:border-primary focus:outline-none"
+      placeholder="비밀번호"
+    />
+    <button
+      class="w-96 rounded bg-primary text-white py-3 hover:bg-dark"
+      @click="onRegister"
+    >
+      회원가입
+    </button>
+    <button class="text-primary">계정이 이미 있으신가요? 로그인 하기</button>
+  </div>
+</template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const email = ref("");
+    const username = ref();
+    const password = ref();
+    const loading = ref(false);
+
+    const onRegister = () => {
+      console.log(email.value, username.value, password.value);
+    };
+
+    return {
+      email,
+      username,
+      password,
+      loading,
+      onRegister,
+    };
+  },
+};
 </script>
 
 <style></style>
