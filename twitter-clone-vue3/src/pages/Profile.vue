5cb4ef4395f5f08ff7a28afd7376731de2a84fd5
BenKang <rksk1588@gmail.com>
2022-08-24 00:46:15 +0900

    2022.08.24

--- twitter-clone-vue3/src/pages/Profile.vue
+++ twitter-clone-vue3/src/pages/Profile.vue
@@ -60,31 +60,45 @@
       <!-- tabs -->
       <div class="flex border-b border-color mt-3">
         <div
-          class="py-3 text-primary font-bold border-b border-primary text-center w-1/4 hover:bg-blue-50 cursor-pointer hover:text-primary"
+          @click="currentTab = 'tweet'"
+          :class="`${
+            currentTab == 'tweet'
+              ? 'border-b border-primary text-primary'
+              : 'text-gray'
+          } py-3 font-bold text-center w-1/3 hover:bg-blue-50 cursor-pointer hover:text-primary`"
         >
           트윗
         </div>
         <div
-          class="py-3 text-gray font-bold text-center w-1/4 hover:bg-blue-50 cursor-pointer hover:text-primary"
+          @click="currentTab = 'reTweet'"
+          :class="`${
+            currentTab == 'reTweet'
+              ? 'border-b border-primary text-primary'
+              : 'text-gray'
+          } py-3 font-bold text-center w-1/3 hover:bg-blue-50 cursor-pointer hover:text-primary`"
         >
-          트윗 및 답글
+          리트윗
         </div>
         <div
-          class="py-3 text-gray font-bold text-center w-1/4 hover:bg-blue-50 cursor-pointer hover:text-primary"
+          @click="currentTab = 'like'"
+          :class="`${
+            currentTab == 'like'
+              ? 'border-b border-primary text-primary'
+              : 'text-gray'
+          } py-3 font-bold text-center w-1/3 hover:bg-blue-50 cursor-pointer hover:text-primary`"
         >
-          미디어
-        </div>
-        <div
-          class="py-3 text-gray font-bold text-center w-1/4 hover:bg-blue-50 cursor-pointer hover:text-primary"
-        >
-          마음에 들어요
+          좋아요
         </div>
       </div>
 
       <!-- tweets -->
       <div class="overflow-y-auto">
         <Tweet
-          v-for="tweet in tweets"
+          v-for="tweet in currentTab == 'tweet'
+            ? tweets
+            : currentTab == 'reTweet'
+            ? reTweets
+            : likeTweets"
           :key="tweet.id"
           :currentUser="currentUser"
           :tweet="tweet"
@@ -102,7 +116,12 @@ import Trends from "../components/Trends.vue";
 import Tweet from "../components/Tweet.vue";
 import store from "../store";
 import { ref, computed, onBeforeMount } from "vue";
-import { TWEET_COLLECTION, USER_COLLECTION } from "../firebase";
+import {
+  LIKE_COLLECTION,
+  RETWEET_COLLECTION,
+  TWEET_COLLECTION,
+  USER_COLLECTION,
+} from "../firebase";
 import getTweetInfo from "../utils/getTweetInfo";
 import moment from "moment";
 
@@ -114,6 +133,9 @@ export default {
   setup() {
     const currentUser = computed(() => store.state.user);
     const tweets = ref([]);
+    const reTweets = ref([]);
+    const likeTweets = ref([]);
+    const currentTab = ref("tweet");
 
     onBeforeMount(() => {
       USER_COLLECTION.doc(currentUser.value.uid).onSnapshot((doc) => {
@@ -138,12 +160,55 @@ export default {
             }
           });
         });
+
+      RETWEET_COLLECTION.where("uid", "==", currentUser.value.uid)
+        .orderBy("created_at", "desc")
+        .onSnapshot((snapshot) => {
+          snapshot.docChanges().forEach(async (change) => {
+            const doc = await TWEET_COLLECTION.doc(
+              change.doc.data().from_tweet_id
+            ).get();
+
+            let tweet = await getTweetInfo(doc.data(), currentUser.value);
+
+            if (change.type === "added") {
+              reTweets.value.splice(change.newIndex, 0, tweet);
+            } else if (change.type === "modified") {
+              reTweets.value.splice(change.oldIndex, 1, tweet);
+            } else if (change.type === "removed") {
+              reTweets.value.splice(change.oldIndex, 1);
+            }
+          });
+        });
+
+      LIKE_COLLECTION.where("uid", "==", currentUser.value.uid)
+        .orderBy("created_at", "desc")
+        .onSnapshot((snapshot) => {
+          snapshot.docChanges().forEach(async (change) => {
+            const doc = await TWEET_COLLECTION.doc(
+              change.doc.data().from_tweet_id
+            ).get();
+
+            let tweet = await getTweetInfo(doc.data(), currentUser.value);
+
+            if (change.type === "added") {
+              likeTweets.value.splice(change.newIndex, 0, tweet);
+            } else if (change.type === "modified") {
+              likeTweets.value.splice(change.oldIndex, 1, tweet);
+            } else if (change.type === "removed") {
+              likeTweets.value.splice(change.oldIndex, 1);
+            }
+          });
+        });
     });
 
     return {
       currentUser,
       tweets,
+      reTweets,
+      likeTweets,
       moment,
+      currentTab,
     };
   },
 };
