84027bea78abd56e737cdb033219cd5347cd5a61
BenKang <rksk1588@gmail.com>
2022-09-01 02:07:17 +0900

    2022.09.01

--- twitter-clone-vue3/src/pages/Notifications.vue
+++ twitter-clone-vue3/src/pages/Notifications.vue
@@ -7,27 +7,28 @@
     <!-- notifications -->
     <div
       class="flex flex-col hover:bg-gray-50 p-3 space-y-2 border-b border-color cursor-pointer"
-      v-for="notification in 10"
-      :key="notification"
+      v-for="notification in notifications"
+      :key="notification.id"
     >
       <div class="flex justify-between">
-        <img
-          src="http://picsum.photos/120"
-          class="w-10 h-10 rounded-full hover:opacity-80"
-        />
-
+        <router-link :to="`/profile/${notification.uid}`">
+          <img
+            :src="notification.profile_image_url"
+            class="w-10 h-10 rounded-full hover:opacity-80"
+          />
+        </router-link>
         <i
           class="fas fa-ellipsis-h text-gray hover:bg-blue-50 hover:text-primary p-2 rounded-full w-10 h-10 flex items-center justify-center"
         ></i>
       </div>
 
-      <div><span class="font-bold">DRX</span> 님의 최근 트윗</div>
-      <div class="text-gray">
-        Presents from fans at LoL PARK and D’IVE last week!🎁 Thank you for your
-        sincere support. (Presents for DRX Pyosik were also delivered.😢) 지난
-        주 롤파크와 D’IVE에서 팬분들이 전해주신 선물!🎁 팬분들의 정성에
-        감사드립니다. (표식 선수에게는 따로 전달했습니다.😢)
+      <div>
+        <span class="font-bold">{{ notification.username }}</span> 님의 최근
+        트윗
       </div>
+      <router-link :to="`/tweet/${notification.id}`" class="text-gray">
+        {{ notification.tweet_body }}
+      </router-link>
     </div>
   </div>
 
@@ -37,10 +38,45 @@
 
 <script>
 import Trends from "../components/Trends.vue";
+import { ref, computed, onBeforeMount } from "vue";
+import store from "../store";
+import { TWEET_COLLECTION } from "../firebase";
+import getTweetInfo from "../utils/getTweetInfo";
 
 export default {
   components: {
     Trends,
+  },
+  setup() {
+    const currentUser = computed(() => store.state.user);
+    const notifications = ref([]);
+
+    onBeforeMount(() => {
+      currentUser.value.followings.forEach(async (following) => {
+        // before 7 days
+        // sec * min * hour * day * ms
+        const dateFrom = Date.now() - 60 * 60 * 24 * 7 * 1000;
+        const snapshot = await TWEET_COLLECTION.where(
+          "created_at",
+          ">",
+          dateFrom
+        )
+          .where("uid", "==", following)
+          .orderBy("created_at", "desc")
+          .get();
+
+        snapshot.docs.forEach(async (doc) => {
+          let tweet = await getTweetInfo(doc.data(), currentUser.value);
+
+          notifications.value.push(tweet);
+        });
+      });
+    });
+
+    return {
+      currentUser,
+      notifications,
+    };
   },
 };
 </script>
