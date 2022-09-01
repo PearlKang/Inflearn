a0da8d07e31da002889fde24d2c5004ef27c3e19
BenKang <rksk1588@gmail.com>
2022-09-02 01:45:10 +0900

    2022.09.02

--- twitter-clone-vue3/src/pages/Messages.vue
+++ twitter-clone-vue3/src/pages/Messages.vue
@@ -6,43 +6,24 @@
         <!-- title -->
         <div class="p-3 font-bold text-lg border-b border-color h-14">쪽지</div>
 
-        <!-- selected -->
+        <!-- a user list -->
         <div
-          class="flex px-3 py-4 hover:bg-gray-50 border-b border-color bg-gray-200"
+          @click="onSelectUser(user)"
+          class="flex items-center cursor-pointer px-3 py-4 hover:bg-gray-50 border-b border-color"
+          v-for="user in users"
+          :key="user.id"
         >
           <img
-            src="http://picsum.photos/200"
+            :src="user.profile_image_url"
             class="w-10 h-10 rounded-full cursor-pointer mr-2"
           />
 
-          <div>
-            <div class="flex space-x-1">
-              <div class="font-bold">benkang.com</div>
-              <div class="text-gray">@benkang</div>
-              <div class="text-gray">7월 20일</div>
+          <div class="flex space-x-2">
+            <div class="font-bold">{{ user.email }}</div>
+            <div class="text-gray">@{{ user.username }}</div>
+            <div class="text-gray">
+              {{ moment(user.created_at).format("M월 DD일") }}
             </div>
-            <div class="text-gray">123123</div>
-          </div>
-        </div>
-
-        <!-- a chat list -->
-        <div
-          class="flex px-3 py-4 hover:bg-gray-50 border-b border-color"
-          v-for="message in 30"
-          :key="message"
-        >
-          <img
-            src="http://picsum.photos/200"
-            class="w-10 h-10 rounded-full cursor-pointer mr-2"
-          />
-
-          <div>
-            <div class="flex space-x-1">
-              <div class="font-bold">benkang.com</div>
-              <div class="text-gray">@benkang</div>
-              <div class="text-gray">7월 20일</div>
-            </div>
-            <div class="text-gray">메세지</div>
           </div>
         </div>
       </div>
@@ -104,11 +85,12 @@
         <!-- chat input -->
         <div class="flex items-center bg-white border-t border-color sticky">
           <input
+            v-model="messageBody"
             type="text"
             class="m-2 py-1 px-4 rounded-full bg-gray-100 resize-none outline-none flex-1"
             placeholder="새 쪽지 작성하기"
           />
-          <button class="text-center">
+          <button class="text-center" @click="onSendMessage">
             <i
               class="far fa-paper-plane text-primary text-lg hover:bg-blue-50 p-2 rounded-full w-10 h-10"
             ></i>
@@ -120,7 +102,90 @@
 </template>
 
 <script>
-export default {};
+import { computed, onBeforeMount, ref } from "vue";
+import { MESSAGE_COLLECTION, USER_COLLECTION } from "../firebase";
+import store from "../store";
+import moment from "moment";
+
+export default {
+  setup() {
+    const currentUser = computed(() => store.state.user);
+    const users = ref([]);
+    const selectedUser = ref(null);
+    const messageBody = ref("");
+    const messages = ref([]);
+
+    onBeforeMount(async () => {
+      const snapshot = await USER_COLLECTION.orderBy(
+        "created_at",
+        "desc"
+      ).get();
+
+      snapshot.docs.forEach((doc) => {
+        let user = doc.data();
+
+        if (user.email === currentUser.value.email) return;
+
+        users.value.push(user);
+      });
+    });
+
+    const onSelectUser = async (user) => {
+      selectedUser.value = user;
+
+      const snapshot = await MESSAGE_COLLECTION.where(
+        "from_uid",
+        "==",
+        currentUser.value.uid
+      )
+        .where("to_uid", "==", selectedUser.value.uid)
+        .orderBy("created_at", "desc")
+        .get();
+
+      messages.value = snapshot.docs.map((doc) => doc.data());
+
+      snapshot = await MESSAGE_COLLECTION.where(
+        "to_uid",
+        "==",
+        currentUser.value.uid
+      )
+        .where("from_uid", "==", selectedUser.value.uid)
+        .orderBy("created_at", "desc")
+        .get();
+
+      snapshot.docs.map((doc) => messages.value.push(doc.data()));
+    };
+
+    const onSendMessage = async () => {
+      if (!messageBody.value || !selectedUser.value) return;
+
+      const doc = MESSAGE_COLLECTION.doc();
+
+      let message = {
+        id: doc.id,
+        from_uid: currentUser.value.uid,
+        to_uid: selectedUser.value.uid,
+        message_body: messageBody.value,
+        created_at: Date.now(),
+      };
+
+      await doc.set(message);
+
+      messages.value.push(message);
+      messageBody.value = "";
+    };
+
+    return {
+      currentUser,
+      users,
+      moment,
+      onSelectUser,
+      onSendMessage,
+      messageBody,
+      messages,
+    };
+  },
+};
 </script>
 
 <style></style>
