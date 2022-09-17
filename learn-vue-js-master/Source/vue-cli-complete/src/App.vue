453ed98feeb83bb9b92e9ec347f7ce1c4a6e5116
BenKang <rksk1588@gmail.com>
2022-09-17 15:10:40 +0900

    2022.09.17

--- learn-vue-js-master/Source/vue-cli-complete/src/App.vue
+++ learn-vue-js-master/Source/vue-cli-complete/src/App.vue
@@ -1,42 +1,27 @@
 <template>
   <div>
-    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
-    <app-header 
-      v-bind:propsdata="str"
-      v-on:renew="renewStr"></app-header>
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue';
-
-// var AppHeader = {
-//   template: '<header><h1>Header</h1></header>'
-// }
-
-// new Vue({
-//   data: {
-//     str: 'hi'
-//   }
-// })
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
-  data: function() {
+  data: function () {
     return {
-      str: 'Header'
-    }
+      str: "Header",
+    };
   },
   components: {
-    'app-header': AppHeader
+    "app-header": AppHeader,
   },
   methods: {
-    renewStr: function() {
-      this.str = 'hi';
-    }
-  }
-}
+    renewStr: function () {
+      this.str = "hi";
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- learn-vue-js-master/Source/vue-form-complete/src/App.vue
+++ learn-vue-js-master/Source/vue-form-complete/src/App.vue
@@ -2,47 +2,45 @@
   <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text" v-model="username">
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password" v-model="password">
+      <input id="password" type="password" v-model="password" />
     </div>
     <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  data: function() {
+  data: function () {
     return {
-      username: '',
-      password: '',
-    }
+      username: "",
+      password: "",
+    };
   },
   methods: {
-    submitForm: function() {
-      // event.preventDefault();
+    submitForm: function () {
       console.log(this.username, this.password);
-      var url = 'https://jsonplaceholder.typicode.com/users';
+      var url = "https://jsonplaceholder.typicode.com/users";
       var data = {
         username: this.username,
-        password: this.password
-      }
-      axios.post(url, data)
-        .then(function(response) {
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function (response) {
           console.log(response);
         })
-        .catch(function(error) {
+        .catch(function (error) {
           console.log(error);
         });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
