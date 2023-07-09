534388bc3b7a68bd83bdc745f92f7d537243fda8
BenKang <rksk1588@gmail.com>
2023-07-09 17:18:54 +0900

    2023.07.09

--- game-with-phaser/phaser3-project-template/src/scenes/PlayingScene.js
+++ game-with-phaser/phaser3-project-template/src/scenes/PlayingScene.js
@@ -30,5 +30,51 @@ export default class PlayingScene extends Phaser.Scene {
 
     // PlayingScene의 background를 설정합니다.
     setBackground(this, "background1");
+
+    this.m_cursorKeys = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    this.movePlayerManager();
+  }
+
+  movePlayerManager() {
+    if (
+      this.m_cursorKeys.left.isDown ||
+      this.m_cursorKeys.right.isDown ||
+      this.m_cursorKeys.up.isDown ||
+      this.m_cursorKeys.down.isDown
+    ) {
+      if (!this.m_player.m_moving) {
+        this.m_player.play("player_anim");
+      }
+      this.m_player.m_moving = true;
+    } else {
+      if (this.m_player.m_moving) {
+        this.m_player.play("player_idle");
+      }
+      this.m_player.m_moving = false;
+    }
+
+    // vector를 사용해 움직임을 관리할 것입니다.
+    // vector = [x좌표 방향, y좌표 방향] 입니다.
+    // 왼쪽 키가 눌려있을 때는 vector[0] += -1, 오른쪽 키가 눌려있을 때는 vector[0] += 1 을 해줍니다.
+    // 위/아래 또한 같은 방법으로 벡터를 수정해줍니다.
+    let vector = [0, 0];
+
+    // this.m_player.x -= PLAYER_SPEED // 공개영상에서 진행했떤 것
+    if (this.m_cursorKeys.left.isDown) {
+      vector[0] += -1;
+    } else if (this.m_cursorKeys.right.isDown) {
+      vector[0] += 1;
+    }
+
+    if (this.m_cursorKeys.up.isDown) {
+      vector[1] += -1;
+    } else if (this.m_cursorKeys.down.isDown) {
+      vector[1] += 1;
+    }
+
+    this.m_player.move(vector);
   }
 }
